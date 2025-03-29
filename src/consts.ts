c108d447c9eb52a84d8c7199aa2adee269c4e0a1
sadhu-sanjay <sanjaygoswami60@gmail.com>
2025-03-29 18:08:17 +0530

    updated tags and building

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Airdome - Visualize your airdome live. ';
+export const SITE_DESCRIPTION = 'Visualize inflatable structures like airdome , apartmetns etc... !';
